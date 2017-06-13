548ad1fe0804361e67c5524e94dc74bece0231d3
Santo <stox.raya@gmail.com>
2017-06-13 16:44:31 +0700

    chatbot verify token

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-hello world
+<?php
+$access_token = "EAAEXKID1wxIBABMyFxZB4WJGyz0NifsFQpCoOZCPqxORZAz7Nusbel3OS7IZCjyIqPrA8Eyl6ZCsdkPSfZAFyEeXONSqayAQEF6P6TWBCjWuyJNs0M3ZCUxDMF4iNbcSf3IL02UZCZBqoZCnQsYbVaiczqIhm20ZCnC9oO7kUfZARuTRayw1HYoMPJQG";
+$verify_token = "fb_mv_bot";
+$hub_verify_token = null;
+if(isset($_REQUEST['hub_challenge'])) {
+ $challenge = $_REQUEST['hub_challenge'];
+ $hub_verify_token = $_REQUEST['hub_verify_token'];
+}
+if ($hub_verify_token === $verify_token) {
+ echo $challenge;
+}
 
-hellow HENDI
-
-hello facebook chatbot
+?>
