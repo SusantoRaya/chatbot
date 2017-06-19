1aaae9df8df488027b353d5f78f844a03b3cadc2
Santo <stox.raya@gmail.com>
2017-06-19 15:49:17 +0700

    asd

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ if($message){
             
             $message_to_reply = $result;
             $jsonData = formatText($sender,$message_to_reply);
-            print_r($jsonData); die();
+            file_put_contents("php://stderr", $jsonData);
     }
     
     $url = "https://graph.facebook.com/v2.6/me/messages?access_token=".$access_token;
