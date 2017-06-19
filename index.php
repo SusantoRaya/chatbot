da0e2991f7be108f1556ed70d5281ba38990f772
SusantoRaya <stox.raya@gmail.com>
2017-06-19 16:31:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -31,14 +31,15 @@ if($message){
     }else{
         
             $url = "http://custom.co.id/admin/index.php?r=Generate/chatbot";
-            $data = http_build_query(array('id' => $message));
+            $data = "id=".$message;
+            //http_build_query(array('id' => $message));
             
             $ch = curl_init($url);
             curl_setopt($ch,CURLOPT_POST,1);
             curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
             //curl_setopt($ch,CURLOPT_HTTPHEADER,array('content-type: application/json'));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
+            //curl_setopt($ch,CURLOPT_SSL_VERIFYPEER,false);
             
             $result = curl_exec($ch);
             curl_close($ch);
